6df507d975d3e3e5841d4c30ead91f03c97dd7ec
Michael Rouves <mrouves@student.42angouleme.fr>
2024-12-10 16:39:24 +0100

    safe atoi for args

--- headers/push_swap.h
+++ headers/push_swap.h
@@ -6,13 +6,15 @@
 /*   By: mrouves <mrouves@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 16:04:33 by mrouves           #+#    #+#             */
-/*   Updated: 2024/12/10 16:05:05 by mrouves          ###   ########.fr       */
+/*   Updated: 2024/12/10 16:10:43 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <stdint.h>
+# include <stdbool.h>
 # include <libft.h>
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -6,13 +6,21 @@
 /*   By: mrouves <mrouves@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 16:04:06 by mrouves           #+#    #+#             */
-/*   Updated: 2024/12/10 16:05:46 by mrouves          ###   ########.fr       */
+/*   Updated: 2024/12/10 16:37:45 by mrouves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <push_swap.h>
+
 int	main(int ac, char **av)
 {
-	(void) ac;
+	//t_array_list	astack;
+	//t_array_list	bstack;
+	if (ac < 2)
+		return (0);
+
+	int32_t	x;
+	ft_printf(1,"%d\n%d\n", ft_safe_atoi(av[1], &x), x);
 	(void) av;
 	return (0);
 }
