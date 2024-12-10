dbad1f127d40aeee92fd3006be665f229e859672
Michael Rouves <mrouves@student.42angouleme.fr>
2024-12-10 16:07:35 +0100

    project final setup

--- sources/manda.mk
+++ sources/manda.mk
@@ -6,7 +6,7 @@
 #    By: mykle <mykle@42angouleme.fr>               +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/08 12:47:41 by mykle             #+#    #+#              #
-#    Updated: 2024/12/10 16:01:48 by mrouves          ###   ########.fr        #
+#    Updated: 2024/12/10 16:05:26 by mrouves          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 define TITLE
@@ -22,16 +22,4 @@ export TITLE
 text-style 		= $(shell tput $1)$2$(shell tput sgr0)
 PRINT_TITLE		:= $(shell $(info $(call text-style,setaf 6, $(TITLE))))
 
-SOURCES		:=	manda/main.c\
-				manda/on_init.c\
-				manda/on_event.c\
-				manda/on_update.c\
-				manda/on_clear.c\
-				application/app.c\
-				application/app_hooks.c\
-				application/app_extra.c\
-				parsing/tilemap.c\
-				parsing/tilemap_parsing.c\
-				parsing/tilemap_check.c\
-				collisions/collision_check.c\
-				collisions/collision_grid.c\
+SOURCES		:=	main.c\
